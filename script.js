f3a60847bf30a1e1568fd77285395359b60965da
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-03-21 22:20:38 -0300

    Requisito 2: Correcao texto nao estava sobre a imagem

--- script.js
+++ script.js
@@ -12,18 +12,19 @@ escreverTagTexto.innerText = pegarTagTexto.value;
 textoDigitado();
 
 //https://www.javascripture.com/FileReader
+//https://www.codegrepper.com/code-examples/html/input+type=%22file%22+and+display+image
 
 function uploadImagem(event){
   const uploaded = document.getElementById('meme-insert');
   uploaded.addEventListener('change', function (event) {
-    console.log(uploaded.files);
-    const reader = new FileReader();
-    reader.onload = function () {
-      const image = document.getElementById('meme-image');
-      image.src = reader.result
-    }
-    reader.readAsDataURL(uploaded.files[0]);
-  })
+   console.log(uploaded.files);
+   const reader = new FileReader();
+   reader.onload = function () {
+   const image = document.getElementById('meme-image');
+   image.src = reader.result
+   }
+   reader.readAsDataURL(uploaded.files[0]);
+})
 }
 
 uploadImagem();
