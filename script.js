02c4902063a6b29e30717016533e0de4a8c649f4
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-03-22 11:18:00 -0300

    Requisito 6: Correcao para RGB

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-function textoDigitado(){
-const pegarTagTexto = document.querySelector('#text-input');
-const escreverTagTexto = document.querySelector('#meme-text');
+function textoDigitado() {
+  const pegarTagTexto = document.querySelector('#text-input');
+  const escreverTagTexto = document.querySelector('#meme-text');
 
 pegarTagTexto.addEventListener('input', function(){
 
-escreverTagTexto.innerText = pegarTagTexto.value;
+  escreverTagTexto.innerText = pegarTagTexto.value;
 });
 }
-
 
 textoDigitado();
 
 //https://www.javascripture.com/FileReader
 //https://www.codegrepper.com/code-examples/html/input+type=%22file%22+and+display+image
 
-function uploadImagem(event){
+function uploadImagem(event) {
   const uploaded = document.querySelector('#meme-insert');
   uploaded.addEventListener('change', function (event) {
-   const reader = new FileReader();
-   reader.onload = function () {
-   const image = document.querySelector('#meme-image');
-   image.src = reader.result
-   }
-   reader.readAsDataURL(uploaded.files[0]);
+  const reader = new FileReader();
+  reader.onload = function () {
+  const image = document.querySelector('#meme-image');
+  image.src = reader.result
+  }
+  reader.readAsDataURL(uploaded.files[0]);
 });
 }
 
@@ -35,15 +34,15 @@ function botoesEstilizaContainer(){
   const pegaContainer = document.querySelector('#meme-image-container')
 
   pegaAgua.addEventListener('click', function(){
-    pegaContainer.style.border = '5px double'; 
+    pegaContainer.style.border = '5px double rgb(0, 0, 255)'; 
   });
 
   pegaFogo.addEventListener('click', function(){
-    pegaContainer.style.border = '3px dashed red';
+    pegaContainer.style.border = '3px dashed rgb(255, 0, 0)';
   });
 
   pegaTerra.addEventListener('click', function(){
-    pegaContainer.style.border = '6px groove green'
+    pegaContainer.style.border = '6px groove rgb(0, 128, 0)'
   });
 }
 
