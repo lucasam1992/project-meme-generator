885fc53e06ce1e34d774f7deb37499cc958859e2
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-03-21 23:07:57 -0300

    Requisito 2: Texto sobre imagem novamente erro

--- script.js
+++ script.js
@@ -15,12 +15,11 @@ textoDigitado();
 //https://www.codegrepper.com/code-examples/html/input+type=%22file%22+and+display+image
 
 function uploadImagem(event){
-  const uploaded = document.getElementById('meme-insert');
+  const uploaded = document.querySelector('#meme-insert');
   uploaded.addEventListener('change', function (event) {
-   console.log(uploaded.files);
    const reader = new FileReader();
    reader.onload = function () {
-   const image = document.getElementById('meme-image');
+   const image = document.querySelector('#meme-image');
    image.src = reader.result
    }
    reader.readAsDataURL(uploaded.files[0]);
